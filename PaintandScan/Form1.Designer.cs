59cae1534ee404b879b1caf5dd1c8e0a5fd339e9
Student <Student>
2016-12-09 17:22:35 +0300

    Сделал пробную рабочую версию

---
+++ PaintandScan/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace PaintandScan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(319, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "20";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(426, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "20";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(319, 73);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(240, 222);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(426, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(507, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(533, 13);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "15";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 325);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ PaintandScan/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintandScan
+{
+    public partial class Form1 : Form
+    {
+        private bool[,] Matr;
+        private int w;
+        private int h;
+        private Graphics graph;
+        private SolidBrush brush;
+        private int rez = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text!="" && textBox2.Text!=""){
+                w = Int32.Parse(textBox1.Text);
+                h = Int32.Parse(textBox2.Text);
+                Matr = new bool[w, h];
+                pictureBox1.Enabled = true;
+                graph=pictureBox1.CreateGraphics();
+                brush = new SolidBrush(Color.White);
+                graph.FillRectangle(brush, 0, 0, w*rez, h*rez);
+                brush.Color = Color.Red;
+                this.rez = Int32.Parse(textBox4.Text);
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //textBox3.Text = e.X.ToString() + " " + e.Y.ToString();
+            //return;
+            if (e.Button == MouseButtons.Left)
+            {
+                int r = 10;
+                int x = e.X;// e.X/rez;// -pictureBox1.Location.X;
+                int y = e.Y;// e.Y / rez;// -pictureBox1.Location.Y;
+                if (x < 0 || y < 0)
+                    return;
+                if ((x/rez) >= w || (y/rez) >= h)
+                    return;
+                graph.FillEllipse(brush, (x - r), (y - r), 2 * r, 2 * r);
+                Matr[x/rez,y/rez] = true;
+                System.IO.File.WriteAllText("tmp.txt",this.convMatrToStr());
+                //textBox3.Text = this.convMatrToStr();
+            }
+        }
+        private string convMatrToStr()
+        {
+            string str = "";
+            for (int j = 0; j <h; j++)
+            {
+                for (int i = 0; i < w; i++)
+                {
+                    if (Matr[i, j])
+                    {
+                        str += "1";
+                    }
+                    else
+                    {
+                        str += " ";
+                    }
+                    str += " ";
+                }
+                str += System.Environment.NewLine;
+            }
+            return str;
+        }
+        private int Convex(Point[] p, int n)
+        {
+            int CONVEX = 1;
+            int  CONCAVE = -1;
+            int i, j, k;
+            int flag = 0;
+            double z;
+
+            if (n < 3)
+                return (0);
+
+            for (i = 0; i < n; i++)
+            {
+                j = (i + 1) % n;
+                k = (i + 2) % n;
+                z = (p[j].X - p[i].X) * (p[k].Y - p[j].Y);
+                z -= (p[j].Y - p[i].Y) * (p[k].X - p[j].X);
+                if (z < 0)
+                    flag |= 1;
+                else if (z > 0)
+                    flag |= 2;
+                if (flag == 3)
+                    return (CONCAVE);
+            }
+            if (flag != 0)
+                return (CONVEX);
+            else
+                return (0);
+        }
+
+        private bool Scan1()
+        {
+            string str = "";
+            int kol = 0;
+            for (int i = 0; i < w; i++)
+                for (int j = 0; j < h; j++)
+                    if (Matr[i, j])
+                        kol++;
+            Point[] mass = new Point[kol*2];
+            int k = 0;
+            Point tmp=new Point();
+            for (int x1 = 0; x1 < w; x1++)
+            {
+                for (int y1 = 0; y1 < h; y1++)
+                {
+                    if (Matr[x1, y1])
+                    {
+                        tmp = new Point(x1, y1);
+                        break;
+                    }  
+                }
+                if (!tmp.Equals(new Point()))
+                    break;
+            }
+            mass[k++]=tmp;
+            int y = tmp.Y;
+            int x;
+            for (x = tmp.X + 1; x < w; x++)
+            {
+                if (Matr[x, y + 1])
+                {
+                    if (!mass.Contains(new Point(x - 1, y)))
+                        mass[k++] = new Point(x, y);
+                    //mass[k++] = new Point(x, y + 1);
+                    y = y + 1;
+                    continue;
+                }
+                if (Matr[x, y - 1])
+                {
+                    if (!mass.Contains(new Point(x - 1, y)))
+                        mass[k++] = new Point(x, y);
+                    //mass[k++] = new Point(x, y - 1);
+                    y = y - 1;
+                    continue;
+                }
+                if (Matr[x, y + 2])
+                {
+                    if (!mass.Contains(new Point(x - 1, y)))
+                        mass[k++] = new Point(x, y);
+                    //mass[k++] = new Point(x, y + 2);
+                    y = y + 2;
+                    continue;
+                }
+                if (Matr[x, y - 2])
+                {
+                    if (!mass.Contains(new Point(x - 1, y)))
+                        mass[k++] = new Point(x, y);
+                    //mass[k++] = new Point(x, y - 2);
+                    y = y - 2;
+                    continue;
+                }
+            }
+            for (x=mass[k-1].X; x > tmp.X; x--)
+            {
+                if (Matr[x, y + 1])
+                {
+                    if (!mass.Contains(new Point(x - 1, y)))
+                        mass[k++] = new Point(x, y);
+                    //mass[k++] = new Point(x, y + 1);
+                    y = y + 1;
+                    continue;
+                }
+                if (Matr[x, y - 1])
+                {
+                    if (!mass.Contains(new Point(x - 1, y)))
+                        mass[k++] = new Point(x, y);
+                    //mass[k++] = new Point(x, y - 1);
+                    y = y - 1;
+                    continue;
+                }
+                if (Matr[x, y + 2])
+                {
+                    if (!mass.Contains(new Point(x - 1, y)))
+                        mass[k++] = new Point(x, y);
+                   // mass[k++] = new Point(x, y + 2);
+                    y = y + 2;
+                    continue;
+                }
+                if (Matr[x, y - 2])
+                {
+                    if (!mass.Contains(new Point(x - 1, y)))
+                        mass[k++] = new Point(x, y);
+                    //mass[k++] = new Point(x, y - 2);
+                    y = y - 2;
+                    continue;
+                }
+            }
+            for (int i = 0; i < k; i++)
+            {
+                str += " (" +mass[i].X.ToString() + ", " + mass[i].Y.ToString() + ") ";
+            }
+            textBox3.Text = str;
+            int re = Convex(mass, k);
+            return (re==-1);
+
+            //bool[,] tmpM = (bool[,]) Matr.Clone();
+            //int x = 0;
+            //for (; x < w; x++)
+            //{
+            //    Point pp;
+            //    for (int y1 = 0; y1 < h; y1++)
+            //        if (Matr[x, y1])
+            //            pp = new Point(x, y1);
+            //    if (pp == null)
+            //        continue;
+
+            //    for (int x1 = x + 1; x1 < w; x1++)
+            //    {
+            //        bool tr = false;
+            //        for (int y = 0; y < h; y++)
+            //        {
+            //            if (Matr[x, y])
+            //            {
+            //                tr = true;
+            //                if (pp.Y == y)
+            //                    continue;
+            //                double six = Math.Abs(pp.X - x) / Math.Abs(pp.Y - y);
+            //                int tmpy = pp.Y;
+            //                for (int tmpx = pp.X; tmpx != x; tmpx++)
+            //                {
+
+            //                }
+
+            //            }
+            //            tr = true;
+
+            //            if (tr)
+            //                tmpM[x, y] = true;
+
+            //        }
+            //    }
+
+                
+            //}
+
+        }
+
+        private bool Scan2()
+        {
+            
+            for (int x = 0; x < w/2; x++)
+            {
+                bool prev = false;
+                bool tr = false;
+                for (int y = 0; y < h; y++)
+                {
+                    if (Matr[x, y])
+                    {
+                        if (tr && !prev)
+                        {
+                            return true;
+                        }
+                        prev = true;
+                        tr = true;
+                    }
+                    else
+                    {
+                        prev = false;
+                    }
+                }
+
+            }
+
+            for (int x = w/2; x < w; x++)
+            {
+                bool prev = false;
+                bool tr = false;
+                for (int y = 0; y < h; y++)
+                {
+                    if (Matr[x, y])
+                    {
+                        if (tr && !prev)
+                        {
+                            return true;
+                        }
+                        prev = true;
+                        tr = true;
+                    }
+                    else
+                    {
+                        prev = false;
+                    }
+                }
+
+            }
+
+            return false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Scan1())
+            {
+                label1.Text = "Вогнутое";
+            } else 
+            label1.Text = "Выпуклое";
+        }
+    }
+}
